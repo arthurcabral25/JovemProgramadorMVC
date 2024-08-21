a72cc946ff583792fe6dc19532929de70d60e0c6
arthurcabral25 <arthurcabralesilvapato@gmail.com>
2024-08-20 21:34:24 -0300

    dfa

--- JovemProgramadorMVC/Controllers/AlunoController.cs
+++ JovemProgramadorMVC/Controllers/AlunoController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JovemProgramadorMVC.Data.Repositorio;
+using JovemProgramadorMVC.Data.Repositorio.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JovemProgramadorMVC.Controllers
 {
     public class AlunoController : Controller
     {
+        private readonly IAlunoRepositorio _alunoRepositorio;
+        public AlunoController(IAlunoRepositorio alunoRepositorio)
+        {
+            _alunoRepositorio = alunoRepositorio;
+        }
         public IActionResult Index()
         {
-            return View();
+            var aluno = _alunoRepositorio.BuscarAlunos();
+            return View(aluno);
         }
     }
 }
