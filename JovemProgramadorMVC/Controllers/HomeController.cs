b89356d4410d0b5f587f7b825accf72f785adcc1
arthurcabral25 <arthurcabralesilvapato@gmail.com>
2024-09-10 21:55:54 -0300

    Sla, meti o professor mas n foi sla pq

--- JovemProgramadorMVC/Controllers/HomeController.cs
+++ JovemProgramadorMVC/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace JovemProgramadorMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
