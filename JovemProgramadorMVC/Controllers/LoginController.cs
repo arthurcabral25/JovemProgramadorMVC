a53ee509ae31132747b24366809b61ace290c23e
arthurcabral25 <arthurcabralesilvapato@gmail.com>
2024-09-23 22:55:40 -0300

    sdfsf

---
+++ JovemProgramadorMVC/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using JovemProgramadorMVC.Data.Repositorio.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JovemProgramadorMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IAlunoRepositorio _alunoRepositorio;
+        public LoginController(IAlunoRepositorio alunoRepositorio)
+        {
+            _alunoRepositorio = alunoRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
